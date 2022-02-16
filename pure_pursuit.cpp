98fd3a4abf45a67ccebb109ae86516dbb6832d09
Vansh Verma <99641079+vanshverma01@users.noreply.github.com>
2022-02-15 23:28:02 -0600

    Update pure_pursuit.cpp

--- pure_pursuit.cpp
+++ pure_pursuit.cpp
@@ -2,7 +2,6 @@
 double vectorRAngle(double endX, double endY)
 {
   double gAngle = vectorGAngle(endX, endY);
-  //Finds angle from 0 - 2pi of the bot angle from the vector angle
   double relativeAngle = angleWrap(gAngle - odom.getAngle(RADIANS), RADIANS);
 
   return relativeAngle;
@@ -10,33 +9,23 @@ double vectorRAngle(double endX, double endY)
 
 double vectorGAngle(double endX, double endY)
 {
-  //Finds X and Y difference from start to target
   double vectorX = endX-odom.getX();
   double vectorY = endY-odom.getY();
-
-  //Finds angle from 0 - 2pi of the vector from 0 (right on the x-axis)
   double vectorGlobalAngle = angleWrap(atan2(vectorY, vectorX), RADIANS);
 
   return vectorGlobalAngle;
 }
-
-
 double vectorLength(double endX, double endY)
 {
-  //Finds X and Y difference from start to target
   double vectorX = endX-odom.getX();
   double vectorY = endY-odom.getY();
-  
-  //Finds length of the vector
   double vectorL = hypot(vectorX, vectorY);
 
   return vectorL;
 }
-
 double coordFinderX(double distance, double angle){
   return odometry.getX() + (distance * cos(angle)); 
 }
-
 double coordFinderY(double distance, double angle){
   return odometry.getY() + (distance * sin(angle)); 
 }
